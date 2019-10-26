88ce17f221e174f95e2882429d1b44cd6a5724a6
Lucas Yuji S Aciole <lucas.aciole@gmail.com>
2019-10-26 13:10:47 -0300

    Add connection close handling - passo 4

--- mytcp.py
+++ mytcp.py
@@ -35,8 +35,6 @@ class Servidor:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
             conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no+1)
-            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
-            # fazer aqui mesmo ou dentro da classe Conexao.
 
             if self.callback:
                 self.callback(conexao)
@@ -51,7 +49,7 @@ class Servidor:
 class Conexao:
     def __init__(self, servidor, id_conexao, ack_no):
         self.servidor = servidor
-        self.id_conexao = id_conexao
+        self.id_conexao = id_conexao # (0 src_addr, 1 src_port, 2 dst_addr, 3 dst_port)
         self.callback = None
         self.seq_no = random.randint(0, 0xffff)
         self.send_base = self.seq_no
@@ -59,7 +57,9 @@ class Conexao:
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
 
-        # Envio do pacote SYN+ACK
+        print("Criada conexão com %s usando seq_no %i. ack_no recebido: %i" % (id_conexao[0], self.seq_no, ack_no))
+
+        # Envio do pacote SYN+ACK. HANDSHAKE
         resp_segment = make_header(self.id_conexao[3], self.id_conexao[1], self.seq_no, self.ack_no, FLAGS_SYN + FLAGS_ACK)
         resp_segment = fix_checksum(resp_segment, self.id_conexao[0], self.id_conexao[2])
         self.servidor.rede.enviar(resp_segment, self.id_conexao[0])
@@ -73,15 +73,25 @@ class Conexao:
         # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
-        if (seq_no == self.ack_no):
-            print('recebido payload: %r' % payload)
+        if((flags & FLAGS_FIN) == FLAGS_FIN): # Flag FIN enviada:
+            resp_segment = make_header(self.id_conexao[3], self.id_conexao[1], self.seq_no, seq_no+1, FLAGS_FIN|FLAGS_ACK) + b''
+            resp_segment = fix_checksum(resp_segment, self.id_conexao[0], self.id_conexao[2])
+
+            self.servidor.rede.enviar(resp_segment, self.id_conexao[0])
+            self.callback(self, payload)
+        elif (seq_no == self.ack_no):
+            print('Recebido pacote: %i %i' % (seq_no, ack_no))
 
             self.ack_no += len(payload)
 
             resp_segment = make_header(self.id_conexao[3], self.id_conexao[1], self.seq_no, self.ack_no, FLAGS_ACK)
             resp_segment = fix_checksum(resp_segment, self.id_conexao[0], self.id_conexao[2])
 
+            self.servidor.rede.enviar(resp_segment, self.id_conexao[0])
+
             self.callback(self, payload)
+        else:
+            print('Recebido pacote: %i %i mas esperava pacote %i %i' % (seq_no, ack_no, self.ack_no, ack_no))
 
     # Os métodos abaixo fazem parte da API
 
@@ -103,6 +113,7 @@ class Conexao:
             resp_segment = make_header(self.id_conexao[1], self.id_conexao[3], self.seq_no, self.ack_no, FLAGS_ACK) + dados[packet_num*MSS:(packet_num+1)*MSS]
             resp_segment = fix_checksum(resp_segment, self.id_conexao[0], self.id_conexao[2])
 
+            print('Enviando pacote: %i %i' % (self.seq_no, self.ack_no))
             self.servidor.rede.enviar(resp_segment, self.id_conexao[2])
 
             self.seq_no += len(dados[packet_num*MSS:(packet_num+1)*MSS])
@@ -112,4 +123,9 @@ class Conexao:
         Usado pela camada de aplicação para fechar a conexão
         """
         # TODO: implemente aqui o fechamento de conexão
+        resp_segment = make_header(self.id_conexao[1], self.id_conexao[3], self.seq_no, self.ack_no, FLAGS_FIN)
+        resp_segment = fix_checksum(resp_segment, self.id_conexao[0], self.id_conexao[2])
+
+        print('Enviando pacote: %i %i' % (self.seq_no, self.ack_no))
+        self.servidor.rede.enviar(resp_segment, self.id_conexao[2])
         pass
